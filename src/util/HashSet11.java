102d4110c0630ac3957d5f54b2351807ab8a2cdb
Jacob Raymond <raymond.jacob17@gmail.com>
2021-01-13 15:50:56 -0500

    Abstract redundancy prime in HashSet11 (used to be hardcoded 13, now can easily be changed to increase memory footprint and bytecode costs of adding and removing, but reduce chances of collisions (which is important for the cost of contains).

--- src/util/HashSet11.java
+++ src/util/HashSet11.java
@@ -3,24 +3,25 @@ package util;
 import battlecode.common.MapLocation;
 
 public class HashSet11 {
-    private final int[] indexes = new int[14];
+    private static final int N = 17;
+    private final int[] indexes = new int[N+1];
     private final int[] location = new int[11];
 
     public HashSet11() {
-        for (int i = 0; i < 14; i++) {
+        for (int i = 0; i < N+1; i++) {
             indexes[i] = 0;
         }
     }
 
     public void add(MapLocation loc) {
         int hash = loc.x << 16 | loc.y;
-        int mod = hash % 13;
+        int mod = hash % N;
         int placeIdx = indexes[mod+1];
         for (int i = indexes[mod]; i < placeIdx; i++) {
             if (location[i] == hash)
                 return;
         }
-        for (int i = mod + 1; i < 14; i++)
+        for (int i = mod + 1; i < N+1; i++)
             indexes[i]++;
         for (int i = 10; i > placeIdx; i--)
             location[i] = location[i-1];
@@ -29,7 +30,7 @@ public class HashSet11 {
 
     public boolean contains(MapLocation loc) {
         int hash = loc.x << 16 | loc.y;
-        int mod = hash % 13;
+        int mod = hash % N;
         int end = indexes[mod+1];
         for (int i = indexes[mod]; i < end; i++) {
             if (location[i] == hash)
@@ -40,7 +41,7 @@ public class HashSet11 {
 
     public void remove(MapLocation loc) {
         int hash = loc.x << 16 | loc.y;
-        int mod = hash % 13;
+        int mod = hash % N;
         int end = indexes[mod+1];
         findAndRemove: {
             for (int i = indexes[mod]; i < end; i++) {
@@ -52,7 +53,7 @@ public class HashSet11 {
             // Location not in structure, return;
             return;
         }
-        for (int i = mod+1; i < 14; i++)
+        for (int i = mod+1; i < N+1; i++)
             indexes[i]--;
         for (int i = end; i < 11; i++)
             location[i-1] = location[i];
