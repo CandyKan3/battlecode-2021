5a944e22dbcb7d4ad4faa98f25b29fdc6562c159
Jacob Raymond <raymond.jacob17@gmail.com>
2021-01-17 13:55:28 -0500

    Add message queue to CustomECController Implement/Modify util data structures

--- src/controllers/CustomECController.java
+++ src/controllers/CustomECController.java
@@ -3,32 +3,28 @@ package controllers;
 import battlecode.common.*;
 import communication.MarsNet.IGetDataType;
 import communication.MarsNet.MarsNet;
+import communication.MarsNet.Packet;
 import communication.MarsNet.PacketHandler;
+import util.HashSet11;
+import util.CircQueue;
 
 import java.util.ArrayList;
-import java.util.PriorityQueue;
+import java.util.Objects;
 
 // Contains useful functions and data for all ECs
 public strictfp abstract class CustomECController<E extends Enum<E> & IGetDataType> extends CustomRobotController<E> {
-    public final PriorityQueue<Integer> friendlyEC = new PriorityQueue<>();
-    public final PriorityQueue<MapLocation> enemyEC = new PriorityQueue<>();
-    public final PriorityQueue<MapLocation> neutralEC = new PriorityQueue<>();
+    public final HashSet11 friendlyLocations = new HashSet11();
+    public final HashSet11 enemyLocations = new HashSet11();
     public final ArrayList<Integer> botIDs = new ArrayList<>();
-    public final int ECID = getID();
+
+    private final CircQueue<Packet<E>> messageQueue = new CircQueue<>(10);
+    private Packet<E> currMessage;
+    private Packet<E> nextMessage;
+    private boolean flagLock = false;
+    private boolean sendLock = false;
 
     public CustomECController(MarsNet<E> marsNet) {
         super(marsNet);
-        friendlyEC.add(ECID);
-    }
-
-    public boolean isMaster() {
-        //noinspection ConstantConditions
-        return friendlyEC.peek() == ECID;
-    }
-
-    public int getMaster() {
-        //noinspection ConstantConditions
-        return friendlyEC.peek();
     }
 
     @Override
@@ -54,15 +50,69 @@ public strictfp abstract class CustomECController<E extends Enum<E> & IGetDataTy
     }
 
     public void handleBots(PacketHandler<?, E> ph) {
-        for (int i = 0; i < botIDs.size(); i++) {
-            int botID = botIDs.get(i);
-            if (!canGetFlag(botID)) {
-                botIDs.set(i, botIDs.get(botIDs.size()-1));
-                botIDs.remove(botIDs.size()-1);
-                i--;
-                continue;
+        try {
+            for (int i = 0; i < botIDs.size(); i++) {
+                int botID = botIDs.get(i);
+                if (!canGetFlag(botID)) {
+                    botIDs.set(i, botIDs.get(botIDs.size() - 1));
+                    botIDs.remove(botIDs.size() - 1);
+                    i--;
+                    continue;
+                }
+                marsNet.getAndHandleF(botID, Objects::nonNull, ph);
             }
-            marsNet.getAndHandleSafe(botID, ph);
+        } catch (GameActionException ignore) { } // Exception will never be thrown.
+    }
+
+    public void messageInit() {
+        sendLock = false;
+        flagLock = nextMessage != null;
+        currMessage = nextMessage;
+        nextMessage = null;
+    }
+
+    public void forceNextMessage(Packet<E> p) {
+        if (nextMessage == null)
+            nextMessage = p;
+    }
+
+    public boolean setMessage(Packet<E> p) {
+        if (!flagLock) {
+            currMessage = p;
+            return true;
         }
+        return false;
+    }
+
+    public void queueMessage(Packet<E> p) {
+        messageQueue.push(p);
+    }
+
+    public boolean setMessageLock(Packet<E> p) {
+        if (!flagLock) {
+            currMessage = p;
+            flagLock = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sendMessage() {
+        if (sendLock)
+            return false;
+        sendLock = true;
+        if (currMessage != null) {
+            marsNet.broadcast(currMessage);
+            return true;
+        }
+        Packet<E> toSend = messageQueue.pop();
+        if (toSend != null) {
+            marsNet.broadcast(toSend);
+            return true;
+        }
+        try {
+            setFlag(0);
+        } catch (GameActionException ignore) { }
+        return false;
     }
 }
